ca447f7262e3dd66dd703667ac9159116c13983c
edwardo <pierenriquez97@gmail.com>
2024-05-10 12:46:20 -0500

    deploy_3

---
+++ backend/routing.py
@@ -0,0 +1,6 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+application = ProtocolTypeRouter({
+    #'websocket': AuthMiddlewareStack(URLRouter([re_path(pipe_ws_endpoint_name(), MessageConsumer if OLDER_CHANNELS else MessageConsumer.as_asgi()),])),
+    ##'http': AuthMiddlewareStack(URLRouter(http_routes)),
+})
